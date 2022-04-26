f0f69d80375dd32a616ee25cd20f5a563dfa1a61
Wenceslau Borges <wbaneto@ymail.com>
2022-04-25 21:29:50 -0300

    Nova alteraçao realizada...

--- src/br/com/letscode/ClasseA.java
+++ src/br/com/letscode/ClasseA.java
@@ -4,6 +4,7 @@ public class ClasseA {
 
     public static void main(String[] args) {
         System.out.println("Commit Alterado!!!....");
+        System.out.println("Nova alteração!!!");
     }
 
 }
